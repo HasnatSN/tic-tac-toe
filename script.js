e9386be16362faff65c1396ce876ddaebee62a8d
HasnatSN <hasnat.kontakt@gmail.com>
2022-04-21 23:33:58 +0200

    changes to the script

--- script.js
+++ script.js
@@ -1,141 +1,57 @@
-const gameBoard = document.querySelector(".gameboard");
+
 const playAgainBtn = document.getElementById("replay-btn");
 
-let signOne = "X";
-let signTwo = "O";
-let player = true;
-let winner = "";
-
-let winningPatterns = [
-  false,
-  false,
-  false,
-  false,
-  false,
-  false,
-  false,
-  false,
-  false,
-];
-
-let gameOver = false;
-
-function createBoard() {
-  gameBoard.style.setProperty("--grid-rows", 3);
-  gameBoard.style.setProperty("--grid-cols", 3);
-  for (i = 0; i < 3 * 3; i++) {
-    let cell = document.createElement("div");
-    cell.setAttribute("data-index", i);
-    cell.innerText = "";
-    gameBoard.appendChild(cell).className = "grid-item";
-  }
-  const cells = document.querySelectorAll(".grid-item");
-  assignEventL(cells);
+function createCell(cell, index) {
+  cell = document.createElement("div");
+  cell.setAttribute("data-index", index);
+  cell.innerText = "";
+  return cell;
 }
 
-function botMove(cells) {
-  let availableCells = [];
-  cells.forEach((cell) => {
-    if (!isOccupied(cell)) {
-      availableCells.push(cell);
-    }
-  });
-
-  cell = availableCells[Math.floor(Math.random() * availableCells.length)];
-  cell.textContent = whoClicked();
-  winningPatterns[cell.dataset.index] = true;
-
+function appendCell(cell, index) {
+  oGameBoard.gameBoardDiv.appendChild(cell).className = "grid-item";
+  oGameBoard.board[index] = 
 }
 
-function whoClicked() {
-  player = !player;
-  return player ? signTwo : signOne;
+function currentSign() {
+    return oGameBoard.sign = oGameBoard.sign === "X" ? "O" : "X";
 }
 
-function isOccupied(cell) {
-  if (cell.textContent != "") return true;
-  return false;
+function placeSign(target) {
+    const fields = document.querySelectorAll(".grid-item")
+    console.log(fields[target]);
+    fields[target].textContent = currentSign();
 }
 
-function playAgain() {
-  gameBoard.textContent = "";
-  createBoard();
-  player = true;
-  winningPatterns = [
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-  ];
-  gameOver = false;
+function checkWinner() {
+    
 }
 
-function isGameOver(cell) {
-  cells = document.querySelectorAll(".grid-item");
-
-  switch (true) {
-    case winningPatterns[0] == true &&
-      winningPatterns[1] == true &&
-      winningPatterns[2] == true:
-      winner = cells[1].textContent;
-      console.log(cells);
-      console.log(cells[1].textContent);
-      gameOver = true;
-    case winningPatterns[3] == true &&
-      winningPatterns[4] == true &&
-      winningPatterns[5] == true:
-      gameOver = true;
-    case winningPatterns[6] == true &&
-      winningPatterns[7] == true &&
-      winningPatterns[8] == true:
-      gameOver = true;
-    case winningPatterns[0] == true &&
-      winningPatterns[3] == true &&
-      winningPatterns[6] == true:
-      gameOver = true;
-    case winningPatterns[1] == true &&
-      winningPatterns[4] == true &&
-      winningPatterns[7] == true:
-      gameOver = true;
-    case winningPatterns[2] == true &&
-      winningPatterns[5] == true &&
-      winningPatterns[8] == true:
-      gameOver = true;
-    case winningPatterns[0] == true &&
-      winningPatterns[4] == true &&
-      winningPatterns[8] == true:
-      gameOver = true;
-    case winningPatterns[2] == true &&
-      winningPatterns[4] == true &&
-      winningPatterns[6] == true:
-      gameOver = true;
-  }
+function assignEventL(cell) {
+  cell.addEventListener(
+    "click",
+    () => {
+      placeSign(cell.dataset.index);
+      checkWinner();
+    },
+    { once: true }
+  );
 }
 
-function assignEventL(cells) {
-  cells.forEach((cell) => {
-    cell.addEventListener("click", () => {
-      if (!isOccupied(cell)) {
-        sign = whoClicked();
-        cell.textContent = sign;
+const oGameBoard = {
+  board: ["", "", "", "", "", "", "", "", ""],
 
-        botMove(cells);
-        winningPatterns[cell.dataset.index] = true;
-        console.log(gameOver);
-        isGameOver(cell);
-        console.log(gameOver);
-      }
+  sign: "O",
+
+  gameBoardDiv: document.querySelector(".gameboard"),
+
+  createBoard: function () {
+    this.board.forEach((element, index) => {
+      cell = createCell(element, index);
+      appendCell(cell, index);
+      assignEventL(cell);
     });
-  });
-}
+  },
+};
 
-playAgainBtn.addEventListener("click", () => {
-  playAgain();
-});
-
-createBoard();
+oGameBoard.createBoard();

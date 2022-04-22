2c676da950618c06411ad997688674383e996cf3
HasnatSN <hasnat.kontakt@gmail.com>
2022-04-22 04:12:36 +0200

    added the functionality for a draw

--- script.js
+++ script.js
@@ -10,46 +10,56 @@ function appendCell(cell) {
 }
 
 function appendToArray(sign, index) {
-    oGameBoard.board[index] = sign;
+  oGameBoard.board[index] = sign;
 
-    oGameBoard.winningPatterns = oGameBoard.winningPatterns.map((i) => {
-        return i.map((j) => {
-            return j == index ? sign : j;
-        })
-    })
+  oGameBoard.winningPatterns = oGameBoard.winningPatterns.map((i) => {
+    return i.map((j) => {
+      return j == index ? sign : j;
+    });
+  });
 }
 
 function switchSign() {
-    return oGameBoard.sign = oGameBoard.sign === "X" ? "O" : "X";
+  return (oGameBoard.sign = oGameBoard.sign === "X" ? "O" : "X");
 }
 
 function placeSign(target) {
-    const fields = document.querySelectorAll(".grid-item")
-    fields[target].textContent = oGameBoard.sign;
-    appendToArray(oGameBoard.sign, target);
-
+  const fields = document.querySelectorAll(".grid-item");
+  fields[target].textContent = oGameBoard.sign;
+  appendToArray(oGameBoard.sign, target);
 }
 
 function isSameSign(sign) {
-    return sign == oGameBoard.sign;
+  return sign == oGameBoard.sign;
+}
+
+function checkForDraw() {
+  let isEmpty = oGameBoard.board.every(checkEmpty);
+
+  function checkEmpty(element) {
+    return element != "";
+  }
+
+  if (isEmpty) document.querySelector("h1").textContent = `Draw.`;
 }
 
 function showWinnerMsg() {
-    document.querySelector("h1").textContent = `${oGameBoard.sign} won the game!`
+  document.querySelector("h1").textContent = `${oGameBoard.sign} won the game!`;
 }
 
 function gameIsOver() {
-    oGameBoard.eventLOn = false;
-    showWinnerMsg();
+  oGameBoard.eventLOn = false;
+  showWinnerMsg();
 }
 
 function currentGameState() {
-    for (let index = 0; index < oGameBoard.winningPatterns.length; index++) {
-        const pattern = oGameBoard.winningPatterns[index];
-        const result = pattern.every(isSameSign);
-        if (result) gameIsOver();
-    }
-    switchSign();
+  for (let index = 0; index < oGameBoard.winningPatterns.length; index++) {
+    const pattern = oGameBoard.winningPatterns[index];
+    const result = pattern.every(isSameSign);
+    if (result) gameIsOver();
+  }
+  switchSign();
+  checkForDraw();
 }
 
 function assignEventL(cell) {
@@ -64,29 +74,10 @@ function assignEventL(cell) {
 }
 
 function playAgain() {
-    document.querySelector("h1").textContent = `TIC TAC TOE`;
-    oGameBoard.gameBoardDiv.textContent = "";
-    oGameBoard.board = ["", "", "", "", "", "", "", "", ""];
-    oGameBoard.winningPatterns = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-      ];
-    oGameBoard.sign = "X";
-    oGameBoard.eventLOn = true;
-    oGameBoard.gameBoardDiv = document.querySelector(".gameboard");
-    oGameBoard.createBoard();
-}
-
-const oGameBoard = {
-  board: ["", "", "", "", "", "", "", "", ""],
-
-  winningPatterns : [
+  document.querySelector("h1").textContent = `TIC TAC TOE`;
+  oGameBoard.gameBoardDiv.textContent = "";
+  oGameBoard.board = ["", "", "", "", "", "", "", "", ""];
+  oGameBoard.winningPatterns = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -94,7 +85,26 @@ const oGameBoard = {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
+  ];
+  oGameBoard.sign = "X";
+  oGameBoard.eventLOn = true;
+  oGameBoard.gameBoardDiv = document.querySelector(".gameboard");
+  oGameBoard.createBoard();
+}
+
+const oGameBoard = {
+  board: ["", "", "", "", "", "", "", "", ""],
+
+  winningPatterns: [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
   ],
 
   sign: "X",
@@ -114,7 +124,7 @@ const oGameBoard = {
 
 const playAgainBtn = document.getElementById("replay-btn");
 playAgainBtn.addEventListener("click", () => {
-    playAgain();
-  });
+  playAgain();
+});
 
 oGameBoard.createBoard();

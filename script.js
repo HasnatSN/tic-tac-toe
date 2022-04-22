f353c5d1d6a0b7d280d78af6e1b6e77d8125ad7c
HasnatSN <hasnat.kontakt@gmail.com>
2022-04-22 03:16:29 +0200

    refactored the code, focused on the code, so the UX isn't the best this time

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-const playAgainBtn = document.getElementById("replay-btn");
-
 function createCell(cell, index) {
   cell = document.createElement("div");
   cell.setAttribute("data-index", index);
@@ -8,40 +5,101 @@ function createCell(cell, index) {
   return cell;
 }
 
-function appendCell(cell, index) {
+function appendCell(cell) {
   oGameBoard.gameBoardDiv.appendChild(cell).className = "grid-item";
-  oGameBoard.board[index] = 
 }
 
-function currentSign() {
+function appendToArray(sign, index) {
+    oGameBoard.board[index] = sign;
+
+    oGameBoard.winningPatterns = oGameBoard.winningPatterns.map((i) => {
+        return i.map((j) => {
+            return j == index ? sign : j;
+        })
+    })
+}
+
+function switchSign() {
     return oGameBoard.sign = oGameBoard.sign === "X" ? "O" : "X";
 }
 
 function placeSign(target) {
     const fields = document.querySelectorAll(".grid-item")
-    console.log(fields[target]);
-    fields[target].textContent = currentSign();
+    fields[target].textContent = oGameBoard.sign;
+    appendToArray(oGameBoard.sign, target);
+
 }
 
-function checkWinner() {
-    
+function isSameSign(sign) {
+    return sign == oGameBoard.sign;
+}
+
+function showWinnerMsg() {
+    document.querySelector("h1").textContent = `${oGameBoard.sign} won the game!`
+}
+
+function gameIsOver() {
+    oGameBoard.eventLOn = false;
+    showWinnerMsg();
+}
+
+function currentGameState() {
+    for (let index = 0; index < oGameBoard.winningPatterns.length; index++) {
+        const pattern = oGameBoard.winningPatterns[index];
+        const result = pattern.every(isSameSign);
+        if (result) gameIsOver();
+    }
+    switchSign();
 }
 
 function assignEventL(cell) {
   cell.addEventListener(
     "click",
     () => {
-      placeSign(cell.dataset.index);
-      checkWinner();
+      if (oGameBoard.eventLOn) placeSign(cell.dataset.index);
+      currentGameState();
     },
     { once: true }
   );
 }
 
+function playAgain() {
+    document.querySelector("h1").textContent = `TIC TAC TOE`;
+    oGameBoard.gameBoardDiv.textContent = "";
+    oGameBoard.board = ["", "", "", "", "", "", "", "", ""];
+    oGameBoard.winningPatterns = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ];
+    oGameBoard.sign = "X";
+    oGameBoard.eventLOn = true;
+    oGameBoard.gameBoardDiv = document.querySelector(".gameboard");
+    oGameBoard.createBoard();
+}
+
 const oGameBoard = {
   board: ["", "", "", "", "", "", "", "", ""],
 
-  sign: "O",
+  winningPatterns : [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ],
+
+  sign: "X",
+
+  eventLOn: true,
 
   gameBoardDiv: document.querySelector(".gameboard"),
 
@@ -53,5 +111,10 @@ const oGameBoard = {
     });
   },
 };
+
+const playAgainBtn = document.getElementById("replay-btn");
+playAgainBtn.addEventListener("click", () => {
+    playAgain();
+  });
 
 oGameBoard.createBoard();
